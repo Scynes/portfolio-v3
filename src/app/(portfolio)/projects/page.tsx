de7f8b80082be8abd7ef98e68591ea17c624c945
Scynes <dustin.greyfield@gmail.com>
2024-09-02 23:54:43 -0400

    add: filtering projects based on selected technologies implemented. If none selected defaults to showing all.

--- src/app/(portfolio)/projects/page.tsx
+++ src/app/(portfolio)/projects/page.tsx
@@ -14,9 +14,9 @@ export default function Page () {
     return (
         <Grid columns={{ initial: '1fr', sm: '14rem 1fr' }} rows={{ initial: 'auto 1fr', sm: 'auto' }} className={ 'pt-[47px] w-full h-full max-h-[calc(100dvh-43px)]' }>
             <Flex className={ 'border-r border-[rgb(30,45,61)]' } direction={ 'column' }>
-                <Dropdown title={ 'projects filter' } expanded>
+                <Dropdown title={ 'technologies' } expanded>
                     <CheckboxGroup.Root value={ selectedValues } onValueChange={ setSelectedValues } size={ '3' } color={ 'gray' } highContrast className={ 'gap-4' }>
-                    {Object.entries(TECHNOLOGIES).map(([key, Icon]) => (
+                    { Object.entries(TECHNOLOGIES).map(([key, Icon]) => (
                         <CheckboxGroup.Item key={key} value={key} className={'flex gap-6 w-full cursor-pointer'}>
                             <Flex gap={'2'} align={'center'} className={'select-none'}>
                                 <Icon size={'1.5rem'} color={`${selectedValues.includes(key) ? '#486c92' : '#2c445d'}`} />
@@ -25,7 +25,7 @@ export default function Page () {
                                 </Text>
                             </Flex>
                         </CheckboxGroup.Item>
-                    ))}
+                    )) }
                     </CheckboxGroup.Root>
                 </Dropdown>
             </Flex>
@@ -34,8 +34,8 @@ export default function Page () {
                     Currently Filtered
                 </Flex>
                 <Flex wrap={ 'wrap'} justify={ 'center' } gap={ '8' } px={ '4' } py={ '8' } className={ 'flex-1 h-full overflow-y-scroll self-center max-w-7xl no-scrollbar' }>
-                    { PROJECTS.map((project, index) => (
-                        <ProjectPreviewCard key={ index } project={ project } />
+                    { PROJECTS.filter(project => selectedValues.length === 0 || project.technologies.some(tech => selectedValues.includes(tech))).map((project, index) => (
+                        <ProjectPreviewCard key={index} project={project} />
                     )) }
                 </Flex>
             </Flex>

52ae83dc60232fad5cdad5c4c885a673b6bc48ba
Scynes <dustin.greyfield@gmail.com>
2024-09-09 14:08:01 -0400

    fix: potential fix for resume location path not rendering on deployment.

--- src/components/about/resume.tsx
+++ src/components/about/resume.tsx
@@ -4,8 +4,8 @@ export const Resume = () => {
 
     return (
         <Flex className={ 'max-w-3xl max-h-full h-full w-full' }>
-            <object data="/files/resume.pdf" type="application/pdf" className='w-full'>
-                <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+            <object data="https://dustinjs.dev/files/resume.pdf" type="application/pdf" className='w-full'>
+                <p>An error occurred, please view my resume here - <a href="http://africau.edu/images/default/sample.pdf">resume link!</a></p>
             </object>
         </Flex>
     );

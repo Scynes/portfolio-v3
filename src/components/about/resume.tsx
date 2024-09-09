1567ec0eafee353f36a17e7ca7ac8dcf482f30ac
Scynes <dustin.greyfield@gmail.com>
2024-09-09 14:14:49 -0400

    refactor: cremoved previous object tag when rendering the resume and replaced with an iframe for better consistency accross mobile browsers.

--- src/components/about/resume.tsx
+++ src/components/about/resume.tsx
@@ -4,9 +4,9 @@ export const Resume = () => {
 
     return (
         <Flex className={ 'max-w-3xl max-h-full h-full w-full' }>
-            <object data="https://dustinjs.dev/files/resume.pdf" type="application/pdf" className='w-full'>
-                <p>An error occurred, please view my resume here - <a href="http://africau.edu/images/default/sample.pdf">resume link!</a></p>
-            </object>
+            <iframe src="/files/resume.pdf" width="100%" height="100%" style={{ minHeight: '100vh' }}>
+                <p>Error, please directly view the resume here - <a href="/files/resume.pdf">resume!</a></p>
+            </iframe>
         </Flex>
     );
 }

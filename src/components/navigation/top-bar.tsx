1bcc1cea951971205e46c45ce5aca69f5e099bda
Scynes <dustin.greyfield@gmail.com>
2024-09-13 11:24:19 -0400

    add: callout for under construction on landing page.

--- src/components/navigation/top-bar.tsx
+++ src/components/navigation/top-bar.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Box, Flex, Heading } from '@radix-ui/themes';
+import { Box, Callout, Flex, Heading } from '@radix-ui/themes';
 import { usePathname } from 'next/navigation';
 import { TopBarLink } from '@/components/navigation/top-bar-link';
 import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 import { useState } from 'react';
+import { FaExclamationCircle } from "react-icons/fa";
 
 export const TopBar = () => {
 
@@ -20,22 +21,34 @@ export const TopBar = () => {
     ];
 
     return (
-        <Flex className={ `transition-all overflow-hidden fixed border-b border-[#1E2D3D] w-full ${ expanded && 'h-auto' } z-50 flex-col md:flex-row bg-[--sky-2]` }>
-            <Flex className={ `${ expanded && 'border-b sm:border-b-0' } py-3 px-4 w-full md:w-56 sm:border-r border-[#1E2D3D]` } justify={ 'between' }>
-                <Heading weight={ 'regular' } size={ '3' } className={ 'text-[#607B96] text-left' }>dustin-portell</Heading>
-                <Box onClick={ () => setExpanded(!expanded) } className={ 'md:hidden' }>
-                    { expanded ? <RxCross2 size={ '1.5rem' } className={ 'text-[#607B96] cursor-pointer' } /> : <RxHamburgerMenu size={ '1.5rem' } className={ 'text-[#607B96] cursor-pointer' } /> }
-                </Box>
-            </Flex>
-            <Flex className={ 'hidden md:flex' }>
-                { PATHS.map( path => <TopBarLink key={ path.href } title={ path.title } href={ path.href }/> ) }
-            </Flex>
-            <Flex direction={ 'column' } className={ `z-50 md:hidden ${ !expanded && 'h-0' }` }>
-                { PATHS.map( path => 
-                    <Box key={ path.href } className={ 'border-b border-[#1E2D3D]' }>
-                        <TopBarLink title={ path.title } href={ path.href }/> 
+        <Flex position={ 'fixed' } className={ 'flex-1 w-full z-50 ' } direction={ 'column' } gap={ '4' }>
+            <Flex className={ `flex-1 transition-all overflow-hidden border-b border-[#1E2D3D] w-full ${ expanded && 'h-auto' } flex-col md:flex-row bg-[--sky-2]` }>
+                <Flex className={ `${ expanded && 'border-b sm:border-b-0' } py-3 px-4 w-full md:w-56 sm:border-r border-[#1E2D3D]` } justify={ 'between' }>
+                    <Heading weight={ 'regular' } size={ '3' } className={ 'text-[#607B96] text-left' }>dustin-portell</Heading>
+                    <Box onClick={ () => setExpanded(!expanded) } className={ 'md:hidden' }>
+                        { expanded ? <RxCross2 size={ '1.5rem' } className={ 'text-[#607B96] cursor-pointer' } /> : <RxHamburgerMenu size={ '1.5rem' } className={ 'text-[#607B96] cursor-pointer' } /> }
                     </Box>
-                ) }
+                </Flex>
+                <Flex className={ 'hidden md:flex' }>
+                    { PATHS.map( path => <TopBarLink key={ path.href } title={ path.title } href={ path.href }/> ) }
+                </Flex>
+                <Flex direction={ 'column' } className={ `z-50 md:hidden ${ !expanded && 'h-0' }` }>
+                    { PATHS.map( path => 
+                        <Box key={ path.href } className={ 'border-b border-[#1E2D3D]' }>
+                            <TopBarLink title={ path.title } href={ path.href }/> 
+                        </Box>
+                    ) }
+                </Flex>
+            </Flex>
+            <Flex className={ `hidden ${ PATH_NAME === '/' && 'lg:flex'}` } justify={ 'center' }>
+                <Callout.Root variant={ 'surface' } color={ 'amber' }>
+                    <Callout.Icon>
+                        <FaExclamationCircle />
+                    </Callout.Icon>
+                    <Callout.Text>
+                        { 'This is a newly developed portfolio! Some functionality may not yet be fully implemented yet.' }
+                    </Callout.Text>
+                </Callout.Root>
             </Flex>
         </Flex>
     );

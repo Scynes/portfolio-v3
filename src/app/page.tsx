dbf3999d87e6246cd52d1b2e76cd97e17e87a821
Scynes <dustin.greyfield@gmail.com>
2024-09-13 17:57:25 -0400

    add: magic UI implementation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import BoxReveal from '@/components/magicui/box-reveal';
+import GradualSpacing from '@/components/magicui/gradual-spacing';
+import WordPullUp from '@/components/magicui/word-pull-up';
 import { StaggeredText } from '@/components/staggered-text';
 import { Container, Flex, Heading, Text, Button, Link as RadixLink } from '@radix-ui/themes';
 import dynamic from 'next/dynamic';
@@ -13,34 +16,22 @@ export default function Page() {
             <Container maxWidth={ '1400px' } className={ 'z-40' } px={{ initial: '6', sm: '9' }}>
                 <Flex align={ 'center' } justify={ 'between' } className={ 'flex-col lg:flex-row sm:mt-52 gap-8 lg:mt-0' }>
                     <Flex gap={ '2' } direction={ 'column' } className={ 'w-fit' }>
-                        <Text className={ 'entry-title' } size={{ initial: '1', sm: '3' }}>
-                            <StaggeredText delay={ 0 }>
-                                { `Hello, my name is` }
-                            </StaggeredText>
-                        </Text>
-                        <Heading weight={ 'regular' } className={ 'transition-all text-[2.4rem] sm:text-6xl' }>
-                            <StaggeredText delay={ 0.5 }>
-                                { `Dustin Portell` }
-                            </StaggeredText>
-                        </Heading>
-                        <Heading size={{ initial: '4', sm: '7' }} weight={ 'regular' } color={ 'indigo' } className={ 'transition-all' }>
-                            <StaggeredText delay={ 1 }>
-                                { '> Full Stack Developer' }
-                            </StaggeredText>
-                        </Heading>
+                        <WordPullUp className={ 'text-xs text-left' } words={ 'Hello! My name is' } />
+                        <GradualSpacing className='tracking-[-0.1em] transition-all text-left text-[1.9rem] sm:text-6xl' text={ 'Dustin Portell' } />
+                        <WordPullUp className={ 'text-lg sm:text-3xl text-[--indigo-11] font-medium text-left' } words={ '> Full Stack Developer' } />
                         <Flex direction={'column'} mt={'5'} gap={'1'}>
-                            <Text className={'text-[#607B96] transition-all'} size={{ initial: '1', sm: '3' }}>
-                                <StaggeredText delay={ 1.5 }>
+                            <BoxReveal>
+                                <Text className={'text-[#607B96] transition-all'} size={{ initial: '1', sm: '3' }}>
                                     {'// welcome to my portfolio'}
-                                </StaggeredText>
-                            </Text>
-                            <Text className={'text-[#607B96] transition-all'} size={{ initial: '1', sm: '3' }}>
-                                <StaggeredText delay={ 2 }>
+                                </Text>
+                            </BoxReveal>
+                            <BoxReveal>
+                                <Text className={'text-[#607B96] transition-all'} size={{ initial: '1', sm: '3' }}>
                                     {'// you can also view my github page'}
-                                </StaggeredText>
-                            </Text>
-                            <Text size={{ initial: '1', sm: '3' }} className={ 'transition-all' }>
-                                <StaggeredText delay={ 2.25 }>
+                                </Text>
+                            </BoxReveal>
+                            <BoxReveal>
+                                <Text size={{ initial: '1', sm: '3' }} className={ 'transition-all' }>
                                     <span className={'text-[--indigo-11]'}>const </span>
                                     <span className={'text-[--jade-11] font-medium'}>github </span>
                                     <span>= </span>
@@ -49,21 +40,23 @@ export default function Page() {
                                         &#34;https://github.com/Scynes&#34;;
                                         </RadixLink>
                                     </span>
-                                </StaggeredText>
-                            </Text>
+                                </Text>
+                            </BoxReveal>
                         </Flex>
-                        <Flex gap={ '3' } className={ 'flex-col sm:flex-row' } mt={ '5' }>                     
-                            <Button asChild variant={ 'soft' } className={ 'transition-all sm:flex-1 cursor-pointer pointer-events-auto' } size={ '4' }>
-                                <Link href={ '/projects' }>
-                                    View Projects
-                                </Link>
-                            </Button>
-                            <Button asChild variant={ 'outline' } className={ 'transition-all sm:flex-1 cursor-pointer' } size={ '4' }>
-                                <Link href={ '/contact' }>
-                                    { "Let's Chat" }
-                                </Link>
-                            </Button>
-                        </Flex>
+                        <BoxReveal width={ '100%' }>
+                            <Flex gap={ '3' } className={ 'flex-col sm:flex-row' } mt={ '5' }>                     
+                                <Button asChild variant={ 'soft' } className={ 'transition-all sm:flex-1 cursor-pointer pointer-events-auto' } size={ '4' }>
+                                    <Link href={ '/projects' }>
+                                        View Projects
+                                    </Link>
+                                </Button>
+                                <Button asChild variant={ 'outline' } className={ 'transition-all sm:flex-1 cursor-pointer' } size={ '4' }>
+                                    <Link href={ '/contact' }>
+                                        { "Let's Chat" }
+                                    </Link>
+                                </Button>
+                            </Flex>
+                        </BoxReveal>
                     </Flex>
                     <SnakeGame />
                 </Flex>
